b8b3ab123d8c1ad9725d0c08adac3cc799d70a30
Lucas Henrique Ronqui de Souza <lucas.ronqui.souza@gmail.com>
2023-03-09 16:24:22 -0300

    added css helper

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,14 +1,34 @@
+import { Button } from '@components/Button';
 import { GroupCard } from '@components/GroupCard';
 import { Header } from '@components/Header';
 import { Highlight } from '@components/Highlight';
+import { ListEmpty } from '@components/ListEmpty';
+import { useState } from 'react';
+import { FlatList } from 'react-native';
 import { Container } from './styles';
 
 export default function Groups() {
+  const [groups, setGroups] = useState<string[]>([])
+
   return (
     <Container>
       <Header />
       <Highlight title='Turmas' subtitle='jogue com a sua turma' />
-      <GroupCard title='Galera do ignite' />
+
+      <FlatList
+        data={groups}
+        keyExtractor={item => item}
+        renderItem={({item}) => (
+          <GroupCard
+            title={item}
+          />
+        )}
+        contentContainerStyle={groups.length === 0 && {flex: 1}}
+        ListEmptyComponent={() => <ListEmpty message='Que tal cadastrar a primeira turma?' />}
+      />
+      <Button
+        title='Criar nova turma'
+      />
     </Container>
   );
 }
